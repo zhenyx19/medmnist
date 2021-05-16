d64d6f77dee7022491d726eb43ea95531e7e90ec
zhenyx19 <81055002+zhenyx19@users.noreply.github.com>
2021-05-16 09:49:07 +0800

    Update models.py

--- medmnist/models.py
+++ medmnist/models.py
@@ -77,8 +77,9 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Sequential(nn.Linear(512 * block.expansion, num_classes),
-                                    nn.Dropout(p = 0.5)
+        self.linear = nn.Sequential(nn.Dropout(p = 0.5),
+                                    nn.Linear(512 * block.expansion, num_classes),
+                                    
                                    )
 
     def _make_layer(self, block, planes, num_blocks, stride):
