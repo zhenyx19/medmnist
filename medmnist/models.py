487015917d02d6a934b56ecee9b9ea42973963d1
zhenyx19 <81055002+zhenyx19@users.noreply.github.com>
2021-05-16 09:36:58 +0800

    Add files via upload

--- medmnist/models.py
+++ medmnist/models.py
@@ -77,17 +77,8 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Sequential(nn.BatchNorm1d(512 * block.expansion),
-                                    nn.Dropout(p = 0.8),
-                                    nn.Linear(512 * block.expansion, 10240),
-                                    nn.BatchNorm1d(10240),
-                                    nn.ReLU(),
-                                    nn.Dropout(p = 0.8),
-                                    nn.Linear(10240,2048),
-                                    nn.BatchNorm1d(2048),
-                                    nn.ReLU(),
-                                    nn.Dropout(p = 0.8),
-                                    nn.Linear(2048,num_classes)
+        self.linear = nn.Sequential(nn.Dropout(p = 0.2),
+                                    nn.Linear(512 * block.expansion, num_classes)
                                    )
 
     def _make_layer(self, block, planes, num_blocks, stride):
